6a342433034de9252dfcf8cd1a88ab8a206526f1
headsht.100.beatch <headsht.100.beatch@mail.ru>
2021-04-15 01:18:58 +0400

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Premya
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds;
+        SqlDataAdapter adapter;
+        SqlCommandBuilder commandBuilder;
+        string connectionString = @"Data Source=85.236.170.148,444;Initial Catalog=TimeUchet;User ID=IS_18_02;Password =IS_18_02";
+        string sql = "SELECT TimeMonth  FROM NormalTime WHERE TimeMonth>2500";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                adapter = new SqlDataAdapter(sql, connection);
+
+                ds = new DataSet();
+                adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                // делаем недоступным столбец id для изменения
+                
+            }
+
+        }
+
+        private void Zapusk_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                adapter = new SqlDataAdapter(sql, connection);
+
+                ds = new DataSet();
+                adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                // делаем недоступным столбец id для изменения
+
+            }
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
